ca63867d578ee001d4d60af8b38ec2528b73c7ea
Alex Meijer <asure007@gmail.com>
2023-10-24 13:24:45 +0200

    - Fix old style - Preasm script thingy in powershell. - axx files generated - add a few missing refs and bits
    Runs out of memory on dosbox..
    
    Signed-off-by: Alex Meijer <asure007@gmail.com>

---
+++ IMG/img.bat
@@ -0,0 +1,26 @@
+loadw main /t=c:\tmp\wwf /FI /E
+loadw bam /A /t=c:\tmp\wwf /FI /E
+loadw bret /A /t=c:\tmp\wwf /FI /E
+loadw doink /A /t=c:\tmp\wwf /FI /E
+loadw lex /A /t=c:\tmp\wwf /FI /E
+loadw razor /A /t=c:\tmp\wwf /FI /E
+loadw shawn /A /t=c:\tmp\wwf /FI /E
+loadw taker /A /t=c:\tmp\wwf /FI /E
+loadw yoko /A /t=c:\tmp\wwf /FI /E
+loadw misc /A /t=c:\tmp\wwf /FI /E
+cd c:\tmp\wwf
+copy bgndglo.txt + bgndtbl.asm temp.xyz
+copy temp.xyz bgndtbl.asm
+copy bgndglo.txt + bgndpal.asm temp.xyz
+copy temp.xyz bgndpal.asm
+del temp.xyz
+del junkxxxx
+del *.~sm
+del *.~bl
+del l2temp
+echo.
+echo.
+echo.
+echo	done...
+echo.
+
