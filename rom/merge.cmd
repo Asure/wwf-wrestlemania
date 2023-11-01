fc168d5f4e7ff4ef3081cbb64700c83d74cb9a2e
Alex Meijer <asure007@gmail.com>
2023-11-01 13:49:19 +0100

    Cleanup roms/rom dir.
    Signed-off-by: Alex Meijer <asure007@gmail.com>

--- rom/merge.cmd
+++ rom/merge.cmd
@@ -1,7 +1,10 @@
 @echo off
+del *.u54
+del *.u63
+rem mk321u54 63
 rem uncomment and add orig roms to build flat merged bin.
-c:\bin\far\srec_cat -o ROM_ORIG.bin -binary wwf.54 -binary -unsplit 2 0 wwf.63 -binary -unsplit 2 1
-rem copy /b mk321f8.0 + mk321fc.0  mk321u54.bin
-rem copy /b mk321f8.1 + mk321fc.1  mk321u63.bin
-rem del *.0 *.1
+rem c:\bin\far\srec_cat -o ROM_ORIG.bin -binary mk321u54.bin -binary -unsplit 2 0 mk321u63.bin -binary -unsplit 2 1
+copy /b wwf321f8.0 + wwf321fc.0  wwf_game_rom_l1.30.u54
+copy /b wwf321f8.1 + wwf321fc.1  wwf_game_rom_l1.30.u63
+del *.0 *.1
 
