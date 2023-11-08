74c39e46969190e0d3e877ed3f44e8545791e0ec
Alex Meijer <asure007@gmail.com>
2023-11-08 16:53:59 +0100

    Time to push this code. - Manually fixed all the remaining .seq files. - Game can now sit in attract for sometime. - But becomes unstable after some minutes, and eventually crashes. - Did some reversing in wwf.asm / wwfseq.asm see ROM/ folder.
    Issues:
    Code alignment is off.
    Something is wrong with the proces list i think.
    Can't select players and/or start a game.
    I also don't see any falling fluff objects during attract.
    There are likely some bugs present because of .axx generation.
    We need to debug this thing.
    
    Signed-off-by: Alex Meijer <asure007@gmail.com>

--- dosbox.cmd
+++ dosbox.cmd
@@ -1,4 +1,9 @@
-"C:\Program Files (x86)\DOSBox-0.74\DOSBox.exe" -conf D:\dosbox\tools\c\mywwf\dosbox.conf
+del *.obj
+del *.out
+del *.lst
+"C:\Program Files (x86)\DOSBox-0.74\DOSBox.exe" -noconsole -conf D:\dosbox\tools\c\mywwf\dosbox.conf
 cd rom
-merge.cmd
-joinnew.cmd
+call merge.cmd
+call joinnew.cmd
+rem update the zip for debugging
+zip d:\mame\roms\wwfmania.zip wwf_*
