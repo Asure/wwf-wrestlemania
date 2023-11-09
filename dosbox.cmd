4248867cfc662ba3e3b2b37f67dddbc8a8d77256
Alex Meijer <asure007@gmail.com>
2023-11-09 17:24:44 +0100

    Playing around with fatals. Doink now has working Undertaker fatal with wrong PAL. (DUDU PUNCH)
    Signed-off-by: Alex Meijer <asure007@gmail.com>

--- dosbox.cmd
+++ dosbox.cmd
@@ -2,9 +2,10 @@ del *.obj
 del *.out
 del *.lst
 "C:\Program Files (x86)\DOSBox-0.74\DOSBox.exe" -noconsole -conf D:\dosbox\tools\c\mywwf\dosbox.conf
+del std*.txt
 cd rom
 call merge.cmd
 call joinnew.cmd
 rem update the zip for debugging
 zip d:\mame\roms\wwfmania.zip wwf_*
-del std*.txt
+
